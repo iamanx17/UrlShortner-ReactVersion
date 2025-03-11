b2199b707d25a4d4fc7e0af9a792f3da197f8ab2
Aman Mishra <iamanx17@gmail.com>
2025-03-11 05:21:42 +0000

    adding cors

--- backend/app/constants.py
+++ backend/app/constants.py
@@ -1,2 +1,7 @@
 DATABASE_URL = 'postgresql://urlShortner_owner:npg_3UYiPZmafOh9@ep-muddy-art-a8rnmea7-pooler.eastus2.azure.neon.tech/urlShortner'
 BASE_URL = 'https://url-shortner-react-version-ojqz.vercel.app/'
+
+ALLOWED_HOSTS = [
+    'https://url-shortner-react-version-ojqz.vercel.app/',
+    'http://url-shortner-react-version-ojqz.vercel.app/'
+]
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.main import router
 from app.core.db import init_db
+from app import constants
 
 
 
 init_db()
-app = FastAPI(title='urlShortner App', description='Welcome to urlshortner APIs', version='1.0')
+app = FastAPI(title='urlShortner App', description='Welcome to urlshortner APIs', summary='Developed by @iamanx17', version='1.0')
+app.add_middleware(CORSMiddleware, allow_origins=constants.ALLOWED_HOSTS, allow_credentials = ['*'], allow_methods=['*'], allow_headers=['*'] )
+
 app.include_router(router)

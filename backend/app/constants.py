64fe0613b590f58e3d2871fe3ead04d8f57fd940
Aman Mishra <iamanx17@gmail.com>
2025-03-11 04:14:24 +0000

    removing unncessary files

--- backend/app/constants.py
+++ backend/app/constants.py
@@ -1,2 +1,2 @@
-DATABASE_URL = 'postgresql://iamanx17:npg_GzPtiq7olWX2@ep-holy-paper-a6h1sw91-pooler.us-west-2.aws.neon.tech/urlShortner?sslmode=requires'
+DATABASE_URL = 'postgresql://iamanx17:npg_GzPtiq7olWX2@ep-holy-paper-a6h1sw91-pooler.us-west-2.aws.neon.tech/urlShortner'
 BASE_URL = 'https://url-shortner-react-version-ojqz.vercel.app/'

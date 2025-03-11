6fc9d102a625ae5f21165cf8bd49a8a51779f498
Aman Mishra <iamanx17@gmail.com>
2025-03-11 04:58:50 +0000

    adding postgresl

--- backend/app/constants.py
+++ backend/app/constants.py
@@ -1,2 +1,2 @@
-DATABASE_URL = 'sqlite:///database.db'
+DATABASE_URL = 'postgresql://urlShortner_owner:npg_3UYiPZmafOh9@ep-muddy-art-a8rnmea7-pooler.eastus2.azure.neon.tech/urlShortner'
 BASE_URL = 'https://url-shortner-react-version-ojqz.vercel.app/'
